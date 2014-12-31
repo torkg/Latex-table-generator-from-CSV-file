c8f73008485f04c0f75101eb4c7a842c0d7e085d
Tor <tgr036@post.uit.no>
2014-12-31 14:47:27 +0100

    Update dataCSV_to_tex_table.py

--- dataCSV_to_tex_table.py
+++ dataCSV_to_tex_table.py
@@ -6,15 +6,18 @@ import csv
 
 #===================
 # GIVE READFILE
-#===================
-# angi: 
-# - filen det skal leses fra 
-# - paa hvilken linje dataene starter og slutter
-# - forste og siste kollonne som skal leser inn
+#=================== 
+# - give start and stop line
+# - first and last col be read
 
-file = 'xy.dat'; data_startline = 0; data_stopline = -1; col_start = 0; col_stop = 1; caption = "Raadata til forsok 2"; label = "tab: forsok2raw"
-
-#file = 'forsok1_1.csv'; data_startline = 13; data_stopline = -1; col_start = 2; col_stop = 6; delimiter = ';' ;caption = "Raadata til forsok 2"; label = "tab: forsok2raw"
+file = 'forsok1_1.csv'
+data_startline = 13
+data_stopline = -1
+col_start = 2
+col_stop = 5
+delimiter = ';'
+caption = "Raadata til forsok 2"
+label = "tab: forsok2raw"
 
 #file = 'forsok1_1 (copy).csv'; data_startline = 13; data_stopline = -1; col_start = 2; col_stop = 6; delimiter = ';' ;caption = "Raadata til forsok 2"; label = "tab: forsok2raw"
 
@@ -29,30 +32,14 @@ file = 'xy.dat'; data_startline = 0; data_stopline = -1; col_start = 0; col_stop
 
 
 #======================================
-# READ RAW: leser inn infile og lukker filen etterpa
-#=====================================
-def ReadRAW( file ):
-    infile = open(file, "r")
-    lines = infile.readlines()
-    infile.close()
-    
-    for line in lines:
-        print line
-    return lines
-
-#======================================
-# END READ RAW
-#======================================
-
-
-#======================================
 # READCSV:Leser inn CSV-fil
 #=====================================
 def ReadCSV( file ):
 
-    #---------------------------------
-    # Blokk som fjerner NULL byte Error
-    #---------------------------------
+    #--------------------------------------
+    # Block to revmove NULL byte Error char
+    # This block is ruining the readfile after
+    #--------------------------------------
     infile = open(file, 'rb')
     data = infile.read()
     infile.close()
@@ -65,15 +52,12 @@ def ReadCSV( file ):
     #---------------------------------
 
     #---------------------------------
-    # Her starter innlesningen av filen
+    # Starting to read the file
     #-----------------------------------
     lines = []
     with open(file, 'rb') as csvfile:
-        
         spamreader = csv.reader(csvfile, delimiter=';')
-                
         for row in spamreader:
-#            print row
             lines.append( row )
     #------------------------------------
 
@@ -83,23 +67,17 @@ def ReadCSV( file ):
 # END READCSV
 #=====================================
 
-
-if file[-3:] == "csv":
-    lines = ReadCSV(file)
-    csv = 1
-else:
-    lines = ReadRAW(file)
-
-
 #==================================================
 # GIVE WRITEFILE: Filen det skal skrives til 
 #=================================================
+
+lines = ReadCSV(file)
 
 writefile = "table_test.tex"
 #writefile = file[:-11] + "_table.tex"
 
 
-# apner filen det skal skrives til
+# opens the write file
 #---------------------------------------
 outfile = open(writefile, "w")
 
@@ -118,7 +96,7 @@ def ColNames():
 
     for i in range(0, num_cols):
         
-        # sa lenge det ikke er det siste kolonnenavnet
+        # as long as it is not the last col
         if (i != num_cols-1 ):
             
             if i==0:
@@ -127,13 +105,13 @@ def ColNames():
             else:
                 outfile.write("\multicolumn{1}{c|}{\\textbf{ %s }} & \n" % col_names[i] )
 
-    # for den siste kolonnetitelen
+    # for the last col
         else:
             outfile.write("\multicolumn{1}{c|}{\\textbf{ %s }} \\\\" % col_names[i] )
 
 #=======================================================
 
-# skriver starten pa tabellen
+# writes the tex code to start longtable
 #--------------------------------------------
 num_cols = col_stop - col_start + 1
 nc = " c " * num_cols 
@@ -145,23 +123,25 @@ start_table = """\\begin{center}
 
 outfile.write(start_table)
 
-# skriver ut kolonnetitlene
+# writes the col titles
 if csv:
     col_names = lines[data_startline-1][col_start:col_stop+1]
 
 outfile.write("\n\hline \n")
 
 
-ColNames() # skriver kolonnenavn          
+ColNames() # writes the col names        
 outfile.write("\\hline")  
 outfile.write("\n\endfirsthead \n\n")
 
+# spilt header top text
 split_header_toptext = "Fortsettelse fra forrige side"
 splitpage_header1 = "\multicolumn{%d}{c} \n{{\\bfseries \\tablename\ \\thetable{} %s}} \n\\\\ \n\hline " %(num_cols, split_header_toptext)
 outfile.write(splitpage_header1)
 
 ColNames()
 
+# spilt header bottom text
 split_header_bottomtext = "Fortsetter paa neste side"
 splitpage_header2 = """\n\endhead
 
@@ -173,7 +153,7 @@ splitpage_header2 = """\n\endhead
 outfile.write(splitpage_header2)
 
 #-----------------------------------------------
-# skriver ut dataene i filen linje for linje
+# writes the data to tex file
 #------------------------------------------------
 
 i = 0
@@ -184,13 +164,13 @@ for line in lines[data_startline : data_stopline]:
 
         word = word.strip()
         print word
-        # sa lenge det ikke er siste kolonn
+        # as long as it is not the last line
         if j < col_stop:
 #            outfile.write("%8.3f & " %( float(word) ))
             outfile.write("%s & " %( word ) )
             j += 1
 
-        # til slutt for siste kolonne
+        # the last line
         else:
 #            outfile.write("%8.3f  " % float(word))
             outfile.write("%s" %( word ) )
@@ -204,9 +184,9 @@ for line in lines[data_startline : data_stopline]:
     i += 1
 #------------------------------------------------
 
-# skriver siste linjene til tabellkoden
+# ending the table
 outfile.write("\\end{longtable}\n")
 outfile.write("\\end{center}\n")
 
-# lukker filen 
+# close the write file
 outfile.close()
